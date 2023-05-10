3de3c20eaba79406f539bad077dbab2a51d013fc
vigobin <vgobin@outlook.com>
2023-05-10 10:34:06 -0500

    Initial commit task 8

---
+++ math/linear_algebra/8-ridin_bareback.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Function that performs matrix multiplication"""
+
+
+def mat_mul(mat1, mat2):
+    """Returns a new matrix"""
+    if len(mat1[0]) != len(mat2):
+        return None
+
+    new_matrix = []
+    for i in range(len(mat1)):
+        for j in range(len(mat2[0])):
+            for k in range(len(mat2)):
+                new_matrix[i][j] += mat1[i][k] * mat2[k][j]
+
+    return new_matrix
