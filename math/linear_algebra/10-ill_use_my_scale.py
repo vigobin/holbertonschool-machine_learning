8474100f3251b4752d08edbc3b0f22f2bed0ba61
vigobin <vgobin@outlook.com>
2023-05-10 10:46:20 -0500

    Task 10

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Function that calculates the shape of a numpy.ndarray"""
+import numpy as np
+
+
+def np_shape(matrix):
+    """Returns the shape"""
+    return matrix.shape
