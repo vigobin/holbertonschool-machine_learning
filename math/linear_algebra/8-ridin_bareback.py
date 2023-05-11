f0e7d414178a2120fcf4845b0b7e8c6390bd17f4
vigobin <vgobin@outlook.com>
2023-05-11 13:02:01 -0500

    Fixed new matrix with correct dimensions

--- math/linear_algebra/8-ridin_bareback.py
+++ math/linear_algebra/8-ridin_bareback.py
@@ -7,9 +7,11 @@ def mat_mul(mat1, mat2):
     if len(mat1[0]) != len(mat2):
         return None
 
-    new_matrix = []
+    new_matrix = [[0 for _ in range(len(mat2[0]))] for _ in range(len(mat1))]
+
     for i in range(len(mat1)):
         for j in range(len(mat2[0])):
+            mult = 0
             for k in range(len(mat2)):
                 new_matrix[i][j] += mat1[i][k] * mat2[k][j]
 
