5846380363ad803476270e64379b7a80f46f1edf
vigobin <vgobin@outlook.com>
2023-05-16 16:01:42 -0500

    Task 10

---
+++ math/calculus/10-matisse.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Calculates the derivative of a polynomial"""
+
+
+def poly_derivative(poly):
+    """Returns a list of coefficients representing the
+    derivative of the polynomial"""
