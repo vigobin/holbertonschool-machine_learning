7c64fc0a62a5f0c437ef419595790dacc938153e
vigobin <vgobin@outlook.com>
2023-05-17 12:16:23 -0500

    Recursion depth was exceeded

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -6,7 +6,4 @@ def summation_i_squared(n):
     """Returns the integer value of the sum"""
     if type(n) is not int or n < 1:
         return None
-    elif n == 1:
-        return 1
-    else:
-        return n**2 + summation_i_squared(n - 1)
+    return (n*(n+1)*(2*n+1)) // 6
