ae5b0fa9ae8dcabf54c3f0547443591da49677eb
vigobin <vgobin@outlook.com>
2023-05-17 22:06:38 -0500

    Integral coding

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -5,7 +5,20 @@
 def poly_integral(poly, C=0):
     """Return a new list of coefficients representing
     the integral of the polynomial"""
-    if type(poly) is not list:
+    if type(poly) is not list or len(poly) < 1:
         return None
     if type(C) is not int:
         return None
+
+    integral = [C]
+    for i in range(len(poly)):
+        coef = poly[i] / (i+1)
+        if coef.is_integer():
+            integral.append(int(coef))
+        else:
+            integral.append(coef)
+
+    """ while len(integral) > 1 and integral[-1] == 0:
+        integral.pop() """
+
+    return integral
