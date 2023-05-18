f440a4f90cb75486aad6e19006433cfe60623d8e
vigobin <vgobin@outlook.com>
2023-05-17 21:58:41 -0500

    coded derivative

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -5,3 +5,15 @@
 def poly_derivative(poly):
     """Returns a list of coefficients representing the
     derivative of the polynomial"""
+    if type(poly) is not list or len(poly) < 1:
+        return None
+
+    derivative = []
+    for i in range(1, len(poly)):
+        coef = poly[i] * i
+        derivative.append(coef)
+
+    if len(derivative) == 0:
+        return [0]
+
+    return derivative
