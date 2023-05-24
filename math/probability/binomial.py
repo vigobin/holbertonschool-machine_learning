7e60dde8dbdef3062b1a41a93a185f2951fe3791
vigobin <vgobin@outlook.com>
2023-05-23 22:56:30 -0500

    Task 11 pdf update

--- math/probability/binomial.py
+++ math/probability/binomial.py
@@ -31,3 +31,27 @@ class Binomial:
                 p = float(mean / n)
                 self.n = n
                 self.p = p
+
+    def pmf(self, k):
+        """
+        calculates the value of the PMF for a given number of successes
+        """
+        if type(k) is not int:
+            k = int(k)
+        if k < 0:
+            return 0
+        p = self.p
+        n = self.n
+        q = (1 - p)
+        n_factorial = 1
+        for i in range(n):
+            n_factorial *= (i + 1)
+        k_factorial = 1
+        for i in range(k):
+            k_factorial *= (i + 1)
+        nk_factorial = 1
+        for i in range(n - k):
+            nk_factorial *= (i + 1)
+        binomial_co = n_factorial / (k_factorial * nk_factorial)
+        pmf = binomial_co * (p ** k) * (q ** (n - k))
+        return pmf
