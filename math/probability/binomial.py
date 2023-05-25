fc702d6dca26236888de47461548bf34319a3dce
vigobin <vgobin@outlook.com>
2023-05-24 22:01:13 -0500

    Task 12

--- math/probability/binomial.py
+++ math/probability/binomial.py
@@ -55,3 +55,16 @@ class Binomial:
         binomial_co = n_factorial / (k_factorial * nk_factorial)
         pmf = binomial_co * (p ** k) * (q ** (n - k))
         return pmf
+
+    def cdf(self, k):
+        """
+        calculates the value of the CDF for a given number of successes
+        """
+        if type(k) is not int:
+            k = int(k)
+        if k < 0:
+            return 0
+        cdf = 0
+        for i in range(k + 1):
+            cdf += self.pmf(i)
+        return cdf
