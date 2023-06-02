76ad219d2c025573583f0a5369c343122d70aa07
vigobin <vgobin@outlook.com>
2023-06-02 16:58:08 -0500

    Changed W to 2d array

--- supervised_learning/classification/0-neuron.py
+++ supervised_learning/classification/0-neuron.py
@@ -12,6 +12,6 @@ class Neuron:
             raise TypeError('nx must be an integer')
         if nx < 1:
             raise ValueError('nx must be a positive integer')
-        self.W = np.random.randn(nx)
+        self.W = np.random.randn(1, nx)
         self.b = 0
         self.A = 0
