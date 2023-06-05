470082d3a0bb59348dfe5aa2270526a404ac4d68
vigobin <vgobin@outlook.com>
2023-06-05 09:56:45 -0500

    Task 1

---
+++ supervised_learning/classification/1-neuron.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""Neuron class"""
+
+import numpy as np
+
+
+class Neuron:
+    """Defines a single neuron performing binary classification"""
+    def __init__(self, nx):
+        """Class constructor"""
+        if type(nx) is not int:
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        return self.__W
+
+    @property
+    def b(self):
+        return self.__b
+
+    @property
+    def A(self):
+        return self.__A
