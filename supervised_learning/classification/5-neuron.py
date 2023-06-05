df28521821687ba7a2a9b8dfd3d2513598f92a00
vigobin <vgobin@outlook.com>
2023-06-05 11:35:09 -0500

    Task 5

---
+++ supervised_learning/classification/5-neuron.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+"""Neuron class"""
+
+import numpy as np
+
+
+class Neuron:
+    """Defines a single neuron performing binary classification"""
+    def __init__(self, nx):
+        """Class constructor"""
+        if type(nx) is not int:
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        return self.__W
+
+    @property
+    def b(self):
+        return self.__b
+
+    @property
+    def A(self):
+        return self.__A
+
+    def forward_prop(self, X):
+        """Calculates the forward propagation of the neuron"""
+        fp = np.matmul(self.__W, X) + self.__b
+        self.__A = 1 / (1 + np.exp(-fp))
+        return self.__A
+
+    def cost(self, Y, A):
+        """Calculates the cost of the model using logistic regression"""
+        m = Y.shape[1]
+        cost = (-1 / m) * np.sum(Y * np.log(A) + (1 - Y) * np.log(
+            1.0000001 - A))
+        return cost
+
+    def evaluate(self, X, Y):
+        """Evaluates the neuron’s predictions"""
+        A = self.forward_prop(X)
+        prediction = np.where(A >= 0.5, 1, 0)
+        cost = self.cost(Y, A)
+        return prediction, cost
+
+    def gradient_descent(self, X, Y, A, alpha=0.05):
+        """Calculates one pass of gradient descent on the neuron"""
+        m = Y.shape[1]
+        dZ = A - Y
+        dW = (1 / m) * np.matmul(X, dZ.T)
+        db = (1 / m) * np.sum(dZ)
+
+        self.__W = self.__W - alpha * dW.T
+        self.__b = self.__b - alpha * db
