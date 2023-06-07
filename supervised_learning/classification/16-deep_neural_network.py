8d3c53ab89525f1d9686b1c7ab66cb76599c6786
vigobin <vgobin@outlook.com>
2023-06-07 16:29:44 -0500

    Task 16

---
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""Deep Neural Network"""
+
+import numpy as np
+
+
+class DeepNeuralNetwork:
+    """defines a deep neural network with one hidden layer
+    performing binary classification"""
+
+    def __init__(self, nx, layers):
+        """Class constructor"""
+        if type(nx) is not int:
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+        if type(layers) is not list or len(layers) == 0:
+            raise TypeError('layers must be a list of positive integers')
+
+        weights = {}
+        # previous = {}
+        for i, layer in enumerate(layers, 1):
+            if type(layer) is not int or layer <= 0:
+                raise TypeError('layers must be a list of positive integers')
+            weights['b{}'.format(i)] = np.zeros((layer, 1))
+            weights['W{}'.format(i)] = np.random.randn(
+                layer, nx) * np.sqrt(2 / nx)
+            # previous = layers
+
+        self.L = len(layers)
+        self.cache = {}
+        self.weights = weights
