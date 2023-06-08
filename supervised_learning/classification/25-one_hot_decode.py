da3a18cd7a3641c6ad17b7d55dd2638b2a39ddbb
vigobin <vgobin@outlook.com>
2023-06-07 21:27:48 -0500

    removed blank

--- supervised_learning/classification/25-one_hot_decode.py
+++ supervised_learning/classification/25-one_hot_decode.py
@@ -8,6 +8,6 @@ def one_hot_decode(one_hot):
     """converts a one-hot matrix into a vector of labels"""
     if type(one_hot) is not np.ndarray or len(one_hot.shape) != 2:
         return None
-    
+
     vector = one_hot.transpose().argmax(axis=1)
     return vector
