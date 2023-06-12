01c79e12b6fad4d0ef336caa79aa67a28789dd89
vigobin <vgobin@outlook.com>
2023-06-12 10:14:34 -0700

    Task 1

---
+++ supervised_learning/tensorflow/1-create_layer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Layers"""
+
+import tensorflow as tf
+
+
+def create_layer(prev, n, activation):
+    """Returns: the tensor output of the layer"""
+
+    initializer: tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    layer = tf.layers.dense(prev, n, activation,
+                            kernel_initializer=initializer)
+    return layer
