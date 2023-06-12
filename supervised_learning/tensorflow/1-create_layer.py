de9a8cc77bd5364c1d293f27b27a048aebeba9e5
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 <vgobin@outlook.com>
2023-06-12 10:31:55 -0700

    updated : to =

--- supervised_learning/tensorflow/1-create_layer.py
+++ supervised_learning/tensorflow/1-create_layer.py
@@ -7,7 +7,7 @@ import tensorflow as tf
 def create_layer(prev, n, activation):
     """Returns: the tensor output of the layer"""
 
-    initializer: tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    initializer= tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
     layer = tf.layers.dense(prev, n, activation,
                             kernel_initializer=initializer)
     return layer
