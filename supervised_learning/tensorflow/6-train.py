d9c9ed5457b5d020229263adb63557f416eaf6f4
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 <vgobin@outlook.com>
2023-06-13 12:56:29 -0700

    task 6

---
+++ supervised_learning/tensorflow/6-train.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+"""Function to train a neural network"""
+
+import tensorflow as tf
+calculate_accuracy = __import__('3-calculate_accuracy').calculate_accuracy
+calculate_loss = __import__('4-calculate_loss').calculate_loss
+create_placeholders = __import__('0-create_placeholders').create_placeholders
+create_train_op = __import__('5-create_train_op').create_train_op
+forward_prop = __import__('2-forward_prop').forward_prop
+
+
+def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations,
+          alpha, iterations, save_path="/tmp/model.ckpt"):
+    """Builds, trains, and saves a neural network classifier:"""
+    x, y = create_placeholders(X_train.shape[1], Y_train.shape[1])
+    tf.add_to_collection('x', x)
+    tf.add_to_collection('y', y)
+    y_pred = forward_prop(x, layer_sizes, activations)
+    tf.add_to_collection('y_pred', y_pred)
+    accuracy = calculate_accuracy(y, y_pred)
+    tf.add_to_collection('accuracy', accuracy)
+    loss = calculate_loss(y, y_pred)
+    tf.add_to_collection('loss', loss)
+    train_op = create_train_op(loss, alpha)
+    tf.add_to_collection('train_op', train_op)
+
+    saver = tf.train.Saver()
+    init = tf.global_variables_initializer()
+
+    with tf.Session() as sess:
+        sess.run(init)
+        for i in range(iterations):
+            loss_train = sess.run(loss,
+                                  feed_dict={x: X_train, y: Y_train})
+            accuracy_train = sess.run(accuracy,
+                                      feed_dict={x: X_train, y: Y_train})
+            loss_valid = sess.run(loss,
+                                  feed_dict={x: X_valid, y: Y_valid})
+            accuracy_valid = sess.run(accuracy,
+                                      feed_dict={x: X_valid, y: Y_valid})
+            if (i % 100) is 0:
+                print("After {} iterations:".format(i))
+                print("\tTraining Cost: {}".format(loss_train))
+                print("\tTraining Accuracy: {}".format(accuracy_train))
+                print("\tValidation Cost: {}".format(loss_valid))
+                print("\tValidation Accuracy: {}".format(accuracy_valid))
+            sess.run(train_op, feed_dict={x: X_train, y: Y_train})
+        i += 1
+        loss_train = sess.run(loss,
+                              feed_dict={x: X_train, y: Y_train})
+        accuracy_train = sess.run(accuracy,
+                                  feed_dict={x: X_train, y: Y_train})
+        loss_valid = sess.run(loss,
+                              feed_dict={x: X_valid, y: Y_valid})
+        accuracy_valid = sess.run(accuracy,
+                                  feed_dict={x: X_valid, y: Y_valid})
+        print("After {} iterations:".format(i))
+        print("\tTraining Cost: {}".format(loss_train))
+        print("\tTraining Accuracy: {}".format(accuracy_train))
+        print("\tValidation Cost: {}".format(loss_valid))
+        print("\tValidation Accuracy: {}".format(accuracy_valid))
+        return saver.save(sess, save_path)
