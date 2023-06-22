4adef6ec9616a62a0977a196f59f74049a37cfdc
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 <vgobin@outlook.com>
2023-06-22 14:58:40 -0700

    Task 6

---
+++ supervised_learning/optimization/6-momentum.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Momentum Upgraded"""
+
+import tensorflow as tf
+
+
+def create_momentum_op(loss, alpha, beta1):
+    """Creates the training operation for a neural network in
+    tensorflow using the gradient descent with
+    momentum optimization algorithm"""
+    return tf.train.MomentumOptimizer(alpha, beta1).minimize(loss)
