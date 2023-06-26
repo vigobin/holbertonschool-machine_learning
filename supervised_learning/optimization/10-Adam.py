25f8cc870e128e359b9f719b756b43eeeb5ab72d
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 <vgobin@outlook.com>
2023-06-26 08:50:44 -0700

    comma

--- supervised_learning/optimization/10-Adam.py
+++ supervised_learning/optimization/10-Adam.py
@@ -7,6 +7,6 @@ import tensorflow as tf
 def create_Adam_op(loss, alpha, beta1, beta2, epsilon):
     """Creates the training operation for a neural network in
     tensorflow using the Adam optimization algorithm"""
-    op = tf.train.AdamOptimizer(alpha, beta1=beta1,
+    op = tf.train.AdamOptimizer(alpha, beta1=beta1, beta2=beta2,
                                 epsilon=epsilon).minimize(loss)
     return op
