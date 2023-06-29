8bec79d09bbd83d9c02117dd6d62ba42e7884972
vigobin <vgobin@outlook.com>
2023-06-28 21:49:39 -0500

    lambtha was not in there

--- supervised_learning/regularization/2-l2_reg_cost.py
+++ supervised_learning/regularization/2-l2_reg_cost.py
@@ -4,7 +4,7 @@
 import tensorflow as tf
 
 
-def l2_reg_cost(cost, lambtha):
+def l2_reg_cost(cost):
     """calculates the cost of a neural network with L2 regularization"""
     l2_reg_cost = tf.losses.get_regularization_losses()
     return (cost + l2_reg_cost)
