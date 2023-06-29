da3252006040d46164b6ceee31d31a51991858ef
vigobin <vgobin@outlook.com>
2023-06-28 21:45:54 -0500

    Corrected

--- supervised_learning/regularization/2-l2_reg_cost.py
+++ supervised_learning/regularization/2-l2_reg_cost.py
@@ -6,8 +6,5 @@ import tensorflow as tf
 
 def l2_reg_cost(cost, lambtha):
     """calculates the cost of a neural network with L2 regularization"""
-    regularizer = tf.reduce_sum(
-        [tf.nn.l2_loss(var) for var in tf.trainable_variables()])
-    l2_loss = lambtha * regularizer
-    total_cost = cost + l2_loss
-    return total_cost
+    l2_reg_cost = tf.losses.get_regularization_losses()
+    return (cost + l2_reg_cost)
