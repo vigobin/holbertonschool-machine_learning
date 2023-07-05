b329e7bcf98387dc44926d7c53911ddd26d29f1c
vigobin <vgobin@outlook.com>
2023-07-05 16:04:25 -0500

    incorrect loop

--- supervised_learning/regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/regularization/1-l2_reg_gradient_descent.py
@@ -8,17 +8,17 @@ def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
     """Updates the weights and biases of a neural network using
     gradient descent with L2 regularization"""
     m = Y.shape[1]
-    dZ = cache["A" + str(L)] - Y
-
-    for i in range(L, 0, -1):
-        A_prev = cache["A" + str(i-1)]
-        W = weights["W" + str(i)]
-        b = weights["b" + str(i)]
-        A = cache["A" + str(i)]
-
-        dW = (1 / m) * np.matmul(dZ, A_prev.T) + (lambtha / m) * W
-        db = (1 / m) * np.sum(dZ, axis=1, keepdims=True)
-        dZ = np.matmul(W.T, dZ) * (1 - np.power(A, 2))
-
-        weights["W" + str(i)] = W - alpha * dW
-        weights["b" + str(i)] = b - alpha * db
+    for layer in range(L, 0, -1):
+        A_cur = cache["A{}".format(layer)]
+        A_prev = cache["A{}".format(layer - 1)]
+        if layer == L:
+            dz = (A_cur - Y)
+        else:
+            dz = dAPrevLayer * (1 - np.square(A_cur))
+        W = weights["W{}".format(layer)]
+        l2 = (lambtha/m) * W
+        dW = np.matmul(dz, A_prev.T)/Y.shape[1] + l2
+        db = np.sum(dz, axis=1, keepdims=True)/Y.shape[1]
+        dAPrevLayer = np.matmul(W.T, dz)
+        weights["W{}".format(layer)] -= alpha * dW
+        weights["b{}".format(layer)] -= alpha * db
