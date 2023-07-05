08197bd3cbc384288310cb4a16ca38e4d61f469c
vigobin <vgobin@outlook.com>
2023-07-05 09:48:40 -0500

    Task 2

---
+++ supervised_learning/keras/2-optimize.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""2. Optimize"""
+
+import tensorflow.keras as K
+
+
+def optimize_model(network, alpha, beta1, beta2):
+    """Sets up Adam optimization for a keras model with categorical
+    crossentropy loss and accuracy metrics"""
+    optimizer = K.optimizers.Adam(lr=alpha, beta_1=beta1, beta_2=beta2)
+    network.compile(optimizer=optimizer,
+                    loss='categorical_crossentropy',
+                    metrics=['accuracy'])
