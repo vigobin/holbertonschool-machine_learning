d2be5194f0e83bd097df6ee2d57380f3e094050f
vigobin <vgobin@outlook.com>
2023-07-05 12:29:03 -0500

    Task 1

---
+++ math/convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""Same Convolution"""
+
+import numpy as np
+
+
+def convolve_grayscale_same(images, kernel):
+    """that performs a same convolution on grayscale images"""
+    m, h, w = images.shape
+    kh, kw = kernel.shape
+
+    # Calculate the padding size
+    pad_h = (kh - 1) // 2
+    pad_w = (kw - 1) // 2
+
+    # Pad the images
+    padded_images = np.pad(images, ((0, 0), (pad_h, pad_h), (pad_w, pad_w)),
+                           mode='constant')
+
+    # Initialize the output array
+    output = np.zeros_like(images)
+
+    # Perform the convolution
+    for i in range(h):
+        for j in range(w):
+            output[:, i, j] = np.sum(padded_images[:, i:i+kh, j:j+kw] * kernel,
+                                     axis=(1, 2))
+
+    return output
