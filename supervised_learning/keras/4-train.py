9c09f695599ac280cea47345b262eb94d4f9528f
vigobin <vgobin@outlook.com>
2023-07-05 10:04:49 -0500

    Task 4

---
+++ supervised_learning/keras/4-train.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""4 Train"""
+
+import tensorflow.keras as tf
+
+
+def train_model(network, data, labels, batch_size, epochs,
+                verbose=True, shuffle=False):
+    """trains a model using mini-batch gradient descent:"""
+    return network.fit(data, labels, batch_size=batch_size,
+                       epochs=epochs, verbose=verbose, shuffle=shuffle)
