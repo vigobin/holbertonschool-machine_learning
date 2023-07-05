4fecdd86a44df6853eef64614b8e784b11915004
vigobin <vgobin@outlook.com>
2023-07-05 15:53:19 -0500

    corrected init

--- supervised_learning/regularization/6-dropout_create_layer.py
+++ supervised_learning/regularization/6-dropout_create_layer.py
@@ -6,19 +6,10 @@ import tensorflow as tf
 
 def dropout_create_layer(prev, n, activation, keep_prob):
     """Creates a layer of a neural network using dropout"""
-    initializer = tf.initializers.GlorotUniform()
-    W = tf.Variable(initializer(shape=(n, prev.shape[1])))
-    b = tf.Variable(tf.zeros((n,)))
-
-    if activation == 'tanh':
-        activation_fn = tf.nn.tanh
-    elif activation == 'softmax':
-        activation_fn = tf.nn.softmax
-
-    z = tf.matmul(W, prev) + b
-    a = activation_fn(z)
-    dropout_mask = tf.cast(tf.random.uniform(
-        shape=tf.shape(a)) < keep_prob, dtype=tf.float32)
-    a_dropout = tf.multiply(a, dropout_mask) / keep_prob
-
-    return a_dropout
+    init = tf.contrib.layers.variance_scaling_initializer(mode='FAN_AVG')
+    dp_mask = tf.layers.Dropout(rate=(1-keep_prob))
+    layers = tf.layers.Dense(units=n,
+                             activation=activation,
+                             kernel_initializer=init,
+                             kernel_regularizer=dp_mask)(prev)
+    return layers
