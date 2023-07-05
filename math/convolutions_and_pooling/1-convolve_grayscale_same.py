6b5be831000b9d460ebac01c3aa9214044c2844b
vigobin <vgobin@outlook.com>
2023-07-05 13:57:26 -0500

    padding fix

--- math/convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -7,11 +7,11 @@ import numpy as np
 def convolve_grayscale_same(images, kernel):
     """that performs a same convolution on grayscale images"""
     m, h, w = images.shape
-    kh, kw = kernel.shape[:2]
+    kh, kw = kernel.shape
 
     # Calculate padding sizes
-    ph = (kh - 1) // 2
-    pw = (kw - 1) // 2
+    ph = int(np.ceil(((h - 1) * 1 + kh - h) / 2))
+    pw = int(np.ceil(((w - 1) * 1 + kw - w) / 2))
 
     # Create padded images
     padded_images = np.pad(images, ((0, 0), (ph, ph), (pw, pw)),
