6b25d402a7564f0e24408bc286c7f9130e7928f3
vigobin <vgobin@outlook.com>
2023-07-05 09:58:17 -0500

    use tf

--- supervised_learning/keras/3-one_hot.py
+++ supervised_learning/keras/3-one_hot.py
@@ -1,15 +1,10 @@
 #!/usr/bin/env python3
 """One Hot"""
 
-import numpy as np
+import tensorflow.keras as K
 
 
 def one_hot(labels, classes=None):
     """Converts a label vector into a one-hot matrix"""
-    if classes is None:
-        classes = np.max(labels) + 1
 
-    one_hot_matrix = np.zeros((len(labels), classes))
-    one_hot_matrix[np.arange(len(labels)), labels] = 1
-
-    return one_hot_matrix
+    return K.utils.to_categorical(labels, num_classes=classes)
