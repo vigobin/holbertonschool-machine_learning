dc7171d0745472f4a30b23b27ab88cb3963d96af
vigobin <vgobin@outlook.com>
2023-07-05 11:40:21 -0500

    Task 9

---
+++ supervised_learning/keras/9-model.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Save and Load Model"""
+
+import tensorflow.keras as K
+
+
+def save_model(network, filename):
+    """Saves an entire model to a file"""
+    network.save(filename)
+
+
+def load_model(filename):
+    """Loads an entire model from a file"""
+    return K.models.load_model(filename)
