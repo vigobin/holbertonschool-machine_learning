3b85a4bd7c19990bc475e9399979f64c2e356713
vigobin <vgobin@outlook.com>
2023-07-07 10:44:08 -0500

    task 0

--- supervised_learning/cnn/0-conv_forward.py
+++ supervised_learning/cnn/0-conv_forward.py
@@ -1,32 +1,42 @@
 #!/usr/bin/env python3
-"""Pooling Forward Prop"""
+"""Convolutional Forward Prop"""
 
 import numpy as np
 
 
-def pool_forward(A_prev, kernel_shape, stride=(1, 1), mode='max'):
+def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
     """
-    Performs forward propagation over a pooling layer of a neural network
+    Performs forward propagation over a convolutional layer of a
+        neural network.
+    A_prev: numpy.ndarray of shape (m, h_prev, w_prev, c_prev) containing the
+        output of the previous layer
+    W: numpy.ndarray of shape (kh, kw, c_prev, c_new) containing the kernels
+        for the convolution
+    b: numpy.ndarray of shape (1, 1, 1, c_new) containing the biases applied
+        to the convolution
+    activation: activation function applied to the convolution
+    padding: string indicating the type of padding used: "same" or "valid"
+    stride: tuple of (sh, sw) containing the strides for the convolution
 
-    A_prev: numpy.ndarray of shape (m, h_prev, w_prev, c_prev)
-        containing the output of the previous layer
-    kernel_shape: tuple of (kh, kw) containing the size of the kernel for
-        pooling
-    stride: tuple of (sh, sw) containing the strides for pooling
-    mode: string containing either 'max' or 'avg', indicating whether
-        to perform maximum or average pooling
-
-    Returns: the output of the pooling layer
+    Returns: the output of the convolutional layer
     """
 
     m, h_prev, w_prev, c_prev = A_prev.shape
-    kh, kw = kernel_shape
+    kh, kw, c_prev, c_new = W.shape
     sh, sw = stride
 
-    h_out = int((h_prev - kh) / sh) + 1
-    w_out = int((w_prev - kw) / sw) + 1
+    if padding == "same":
+        ph = int(np.ceil((h_prev - 1) * sh - h_prev + kh) / 2)
+        pw = int(np.ceil((w_prev - 1) * sw - w_prev + kw) / 2)
+        A_prev_pad = np.pad(
+            A_prev, ((0, 0), (ph, ph), (pw, pw), (0, 0)), mode='constant')
+    elif padding == "valid":
+        A_prev_pad = A_prev
 
-    A = np.zeros((m, h_out, w_out, c_prev))
+    h_out = int((h_prev - kh + 2 * ph) / sh) + 1
+    w_out = int((w_prev - kw + 2 * pw) / sw) + 1
+
+    A = np.zeros((m, h_out, w_out, c_new))
 
     for i in range(h_out):
         for j in range(w_out):
@@ -35,11 +45,11 @@ def pool_forward(A_prev, kernel_shape, stride=(1, 1), mode='max'):
             w_start = j * sw
             w_end = w_start + kw
 
-            if mode == 'max':
-                A[:, i, j, :] = np.max(
-                    A_prev[:, h_start:h_end, w_start:w_end, :], axis=(1, 2))
-            elif mode == 'avg':
-                A[:, i, j, :] = np.mean(
-                    A_prev[:, h_start:h_end, w_start:w_end, :], axis=(1, 2))
+            A[:, i, j, :] = np.sum(A_prev_pad[
+                :, h_start:h_end, w_start:w_end,
+                :, np.newaxis] * W, axis=(1, 2, 3))
 
-    return A
+    Z = A + b
+    A_out = activation(Z)
+
+    return A_out
