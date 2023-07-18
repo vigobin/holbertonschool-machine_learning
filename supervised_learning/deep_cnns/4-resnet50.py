65be5978ae10814e77aed7142a512fd99d9c7d81
vigobin <vgobin@outlook.com>
2023-07-18 17:22:34 -0500

    Corrected model

--- supervised_learning/deep_cnns/4-resnet50.py
+++ supervised_learning/deep_cnns/4-resnet50.py
@@ -18,47 +18,44 @@ def resnet50():
 
     Returns: the keras model
 """
-    input_shape = (224, 224, 3)
-    input_layer = K.layers.Input(shape=input_shape)
+    init = K.initializers.he_normal()
+    activation = K.activations.relu
+    img_input = K.Input(shape=(224, 224, 3))
+    C0 = K.layers.Conv2D(filters=64,
+                         kernel_size=(7, 7),
+                         padding='same',
+                         strides=(2, 2),
+                         kernel_initializer=init)(img_input)
+    Batch_NormC0 = K.layers.BatchNormalization(axis=3)(C0)
+    ReLUC0 = K.layers.Activation(activation)(Batch_NormC0)
+    MP1 = K.layers.MaxPooling2D(pool_size=(3, 3),
+                                strides=(2, 2),
+                                padding='same')(ReLUC0)
+    PB2 = projection_block(MP1, [64, 64, 256], s=1)
+    IB3 = identity_block(PB2, [64, 64, 256])
+    IB4 = identity_block(IB3, [64, 64, 256])
 
-    X = K.layers.ZeroPadding2D((3, 3))(input_layer)
+    PB5 = projection_block(IB4, [128, 128, 512], s=2)
+    IB6 = identity_block(PB5, [128, 128, 512])
+    IB7 = identity_block(IB6, [128, 128, 512])
+    IB8 = identity_block(IB7, [128, 128, 512])
 
-    # Stage 1
-    X = K.layers.Conv2D(64, (7, 7), strides=(2, 2),
-                        kernel_initializer='he_normal')(X)
-    X = K.layers.BatchNormalization(axis=3)(X)
-    X = K.layers.Activation('relu')(X)
-    X = K.layers.MaxPooling2D((3, 3), strides=(2, 2))(X)
+    PB9 = projection_block(IB8, [256, 256, 1024], s=2)
+    IB10 = identity_block(PB9, [256, 256, 1024])
+    IB11 = identity_block(IB10, [256, 256, 1024])
+    IB12 = identity_block(IB11, [256, 256, 1024])
+    IB13 = identity_block(IB12, [256, 256, 1024])
+    IB14 = identity_block(IB13, [256, 256, 1024])
 
-    # Stage 2
-    X = projection_block(X, [64, 64, 256], s=1)
-    X = identity_block(X, [64, 64, 256])
-    X = identity_block(X, [64, 64, 256])
+    PB15 = projection_block(IB14, [512, 512, 2048], s=2)
+    IB16 = identity_block(PB15, [512, 512, 2048])
+    IB17 = identity_block(IB16, [512, 512, 2048])
 
-    # Stage 3
-    X = projection_block(X, [128, 128, 512])
-    X = identity_block(X, [128, 128, 512])
-    X = identity_block(X, [128, 128, 512])
-    X = identity_block(X, [128, 128, 512])
-
-    # Stage 4
-    X = projection_block(X, [256, 256, 1024])
-    X = identity_block(X, [256, 256, 1024])
-    X = identity_block(X, [256, 256, 1024])
-    X = identity_block(X, [256, 256, 1024])
-    X = identity_block(X, [256, 256, 1024])
-    X = identity_block(X, [256, 256, 1024])
-
-    # Stage 5
-    X = projection_block(X, [512, 512, 2048])
-    X = identity_block(X, [512, 512, 2048])
-    X = identity_block(X, [512, 512, 2048])
-
-    X = K.layers.AveragePooling2D((2, 2))(X)
-
-    X = K.layers.Flatten()(X)
-    X = K.layers.Dense(units=1000, activation='softmax')(X)
-
-    model = K.models.Model(inputs=input_layer, outputs=X)
-
+    AP18 = K.layers.AveragePooling2D(pool_size=(7, 7),
+                                     strides=(1, 1),
+                                     padding='valid')(IB17)
+    output = K.layers.Dense(1000,
+                            activation='softmax',
+                            kernel_initializer=init)(AP18)
+    model = K.Model(inputs=img_input, outputs=output)
     return model
