265291bdc40a72e0e0a05e8315994e26cbed3987
vigobin <vgobin@outlook.com>
2023-07-25 11:05:15 -0500

    Used DenseNet121

--- supervised_learning/transfer_learning/0-transfer.py
+++ supervised_learning/transfer_learning/0-transfer.py
@@ -15,17 +15,21 @@ def preprocess_data(X, Y):
     return X_p, Y_p
 
 
-def lenet5(X):
-    # Build the LeNet-5 architecture
-    conv1 = K.layers.Conv2D(6, (5, 5), activation='relu', padding='same')(X)
-    pool1 = K.layers.MaxPooling2D((2, 2), strides=(2, 2))(conv1)
-    conv2 = K.layers.Conv2D(16, (5, 5), activation='relu', padding='valid')(
-        pool1)
-    pool2 = K.layers.MaxPooling2D((2, 2), strides=(2, 2))(conv2)
-    flatten = K.layers.Flatten()(pool2)
-    dense1 = K.layers.Dense(120, activation='relu')(flatten)
-    dense2 = K.layers.Dense(84, activation='relu')(dense1)
-    output = K.layers.Dense(10, activation='softmax')(dense2)
+def densenet121():
+    # Build the DenseNet-121 architecture
+    input_shape = (224, 224, 3)
+    base_model = K.applications.DenseNet121(
+        include_top=False, weights='imagenet', input_shape=input_shape)
+
+    # Freeze all layers in the base model
+    for layer in base_model.layers:
+        layer.trainable = False
+
+    X = base_model.output
+    X = K.layers.GlobalAveragePooling2D()(X)
+    X = K.layers.Dense(120, activation='relu')(X)
+    X = K.layers.Dense(84, activation='relu')(X)
+    output = K.layers.Dense(10, activation='softmax')(X)
 
     return output
 
@@ -38,26 +42,31 @@ def main():
     X_train, Y_train = preprocess_data(X_train, Y_train)
     X_test, Y_test = preprocess_data(X_test, Y_test)
 
+    # Resize the images to match DenseNet-121 input shape (224x224)
+    X_train_resized = np.array([K.backend.resize_images(
+        img, (224, 224)) for img in X_train])
+    X_test_resized = np.array([K.backend.resize_images(
+        img, (224, 224)) for img in X_test])
+
     # Create the input layer
-    input_shape = (X_train.shape[1], X_train.shape[2], X_train.shape[3])
+    input_shape = (224, 224, 3)
     X_input = K.Input(shape=input_shape)
 
-    # Create the LeNet-5 model
-    output = lenet5(X_input)
+    # Create the DenseNet-121 model
+    output = densenet121()
 
     # Create the model
     model = K.Model(inputs=X_input, outputs=output)
 
     # Compile the model
     model.compile(
-        optimizer='adam',
-        loss='categorical_crossentropy',
+        optimizer='adam', loss='categorical_crossentropy',
         metrics=['accuracy'])
 
     # Train the model
     model.fit(
-        X_train, Y_train, batch_size=32,
-        epochs=10, validation_data=(X_test, Y_test))
+        X_train_resized, Y_train, batch_size=32, epochs=10,
+        validation_data=(X_test_resized, Y_test))
 
     # Save the model
     model.save('cifar10.h5')
