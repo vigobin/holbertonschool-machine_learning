574883287248c2ac31f16a54911cd929db0189dc
vigobin <vgobin@outlook.com>
2023-09-11 13:05:31 -0500

    used minor value for cofactor

--- math/advanced_linear_algebra/2-cofactor.py
+++ math/advanced_linear_algebra/2-cofactor.py
@@ -21,6 +21,54 @@ def cofactor(matrix):
 
     cofactor_matrix = []
 
+    for i in range(n):
+        cofactor_row = []
+        for j in range(n):
+            sub_matrix = [row[:j] + row[j + 1:] for row in (
+                matrix[:i] + matrix[i + 1:])]
+            minor_value = determinant(sub_matrix)
+            sign = (-1) ** (i + j)
+            cofactor_value = minor_value * sign
+            cofactor_row.append(cofactor_value)
+
+        cofactor_matrix.append(cofactor_row)
+
+    return cofactor_matrix
+
+
+def minor(matrix):
+    """Calculates the minor matrix of a matrix"""
+    if type(matrix) is not list or len(matrix) == 0:
+        raise TypeError("matrix must be a list of lists")
+
+    for i in matrix:
+        if type(i) is not list:
+            raise TypeError("matrix must be a list of lists")
+
+    n = len(matrix)
+    for row in matrix:
+        if len(row) != n:
+            raise ValueError("matrix must be a non-empty square matrix")
+
+    if n == 1:
+        return[[1]]
+
+    minor_matrix = []
+
+    for i in range(n):
+        minor_row = []
+        for j in range(n):
+            # Calculate the minor by removing the i-th row and j-th column
+            #   and finding the determinant.
+            sub_matrix = [row[:j] + row[j + 1:] for row in (
+                matrix[:i] + matrix[i + 1:])]
+            minor_value = determinant(sub_matrix)
+            minor_row.append(minor_value)
+        minor_matrix.append(minor_row)
+
+    return minor_matrix
+
+
 def determinant(matrix):
     """Calculates the determinant of a matrix"""
     if type(matrix) is not list or len(matrix) == 0:
