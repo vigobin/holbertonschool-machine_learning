5ffca28d179a51c30500cc4bb9ad7d6970754435
vigobin <vgobin@outlook.com>
2023-09-11 17:27:50 -0500

    Task 5

---
+++ math/advanced_linear_algebra/5-definiteness.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Definiteness of matrix"""
+
+import numpy as np
+
+def definiteness(matrix):
+    """Calculates the definiteness of a matrix
+        Returns: The string Positive definite, Positive semi-definite,
+            Negative semi-definite, Negative definite, or Indefinite"""
+    
