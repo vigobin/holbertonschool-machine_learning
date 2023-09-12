974a7acfa497948aa637e4da22edda4db462fe99
vigobin <vgobin@outlook.com>
2023-09-12 09:39:20 -0500

    added error check

--- math/multivariate_prob/multinormal.py
+++ math/multivariate_prob/multinormal.py
@@ -3,7 +3,15 @@
 
 import numpy as np
 
+
 class MultiNormal:
     """Represents a Multivariate Normal distribution"""
     def __init__(self, data):
         """Consructor"""
+        if type(data) is not np.ndarray or len(data.shape) != 2:
+            raise TypeError("data must be a 2D numpy.ndarray")
+
+        d, n = data.shape
+
+        if n < 2:
+            raise ValueError("data must contain multiple data points")
