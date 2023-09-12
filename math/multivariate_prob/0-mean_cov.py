b07d478407869dad4267a8822d2f7b6bb4cd0eea
vigobin <vgobin@outlook.com>
2023-09-12 08:40:24 -0500

    error check

--- math/multivariate_prob/0-mean_cov.py
+++ math/multivariate_prob/0-mean_cov.py
@@ -5,5 +5,16 @@ import numpy as np
 
 
 def mean_cov(X):
-    """Calculates the mean and covariance of a data set"""
-    
+    """Calculates the mean and covariance of a data set
+        Returns:
+            mean: numpy.ndarray of shape (1, d) containing the
+                mean of the data set
+            cov: numpy.ndarray of shape (d, d) containing the covariance
+                matrix of the data set"""
+    if type(X) is not np.ndarray:
+        raise TypeError("X must be a 2D numpy.ndarray")
+
+    n, d = X.shape
+
+    if n < 2:
+        raise ValueError("X must contain multiple data points")
