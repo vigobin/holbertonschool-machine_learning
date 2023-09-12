047d553ead273d87f6c2829880f4f4525cfb5558
vigobin <vgobin@outlook.com>
2023-09-12 08:49:08 -0500

    calculate mean cov

--- math/multivariate_prob/0-mean_cov.py
+++ math/multivariate_prob/0-mean_cov.py
@@ -18,3 +18,14 @@ def mean_cov(X):
 
     if n < 2:
         raise ValueError("X must contain multiple data points")
+
+    mean = np.mean(X, axis=0)
+
+    cov = np.zeros((d, d))
+
+    for i in range(d):
+        for j in range(d):
+            cov[i, j] = np.sum((X[:, i] - mean[:, i]) * (
+                X[:, j] - mean[:, j])) / (n - 1)
+
+    return mean, cov
