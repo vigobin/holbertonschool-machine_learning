29c891687096ee7f2a283c3129c76ca54b8ea2ed
vigobin <vgobin@outlook.com>
2023-09-12 09:36:12 -0500

    Task 2

---
+++ math/multivariate_prob/multinormal.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Initialize class Multinormal"""
+
+import numpy as np
+
+class MultiNormal:
+    """Represents a Multivariate Normal distribution"""
+    def __init__(self, data):
+        """Consructor"""
