9ddd6ffb9a0b79f479c59fa5c0cde4750f5b9d5a
vigobin <vgobin@outlook.com>
2023-09-13 13:18:41 -0500

    x not n

--- math/bayesian_prob/0-likelihood.py
+++ math/bayesian_prob/0-likelihood.py
@@ -15,7 +15,7 @@ def likelihood(x, n, P):
             the data, x and n, for each probability in P, respectively"""
     if type(n) is not int or n <= 0:
         raise ValueError("n must be a positive integer")
-    if type(n) is not int or x < 0:
+    if type(x) is not int or x < 0:
         raise ValueError(
             "x must be an integer that is greater than or equal to 0")
     if x > n:
