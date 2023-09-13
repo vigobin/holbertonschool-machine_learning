e0fa0f09f636a793e8f83ac3cf7a8db1a3e18686
vigobin <vgobin@outlook.com>
2023-09-13 14:11:22 -0500

    Task 1

---
+++ math/bayesian_prob/1-intersection.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""Intersection function"""
+
+import numpy as np
+
+
+def intersection(x, n, P, Pr):
+    """Calculates the intersection of obtaining this data with the various
+    hypothetical probabilities.
+        x is the number of patients that develop severe side effects.
+        n is the total number of patients observed.
+        P is a 1D numpy.ndarray containing the various hypothetical
+            probabilities of developing severe side effects.
+        Pr is a 1D numpy.ndarray containing the prior beliefs of P.
+        Returns: a 1D numpy.ndarray containing the intersection of obtaining
+        x and n with each probability in P, respectively."""
+    if type(n) is not int or n <= 0:
+        raise ValueError("n must be a positive integer")
+    if type(x) is not int or x < 0:
+        raise ValueError(
+            "x must be an integer that is greater than or equal to 0")
+    if x > n:
+        raise ValueError("x cannot be greater than n")
+    if type(P) is not np.ndarray or P.ndim != 1:
+        raise TypeError("P must be a 1D numpy.ndarray")
+    if not np.all((P >= 0) & (P <= 1)):
+        raise ValueError('All values in P must be in the range [0, 1]')
+
+    items = np.math.factorial(n)
+    coef = items / (np.math.factorial(x) * np.math.factorial(n - x))
+    likelihoods = coef * (P ** x) * ((1 - P) ** (n - x))
+    return likelihoods
