2ecd178176e37b7fab893ace0a86ec76c0a4301d
vigobin <vgobin@outlook.com>
2023-09-13 13:16:15 -0500

    correct arguments

--- math/bayesian_prob/0-likelihood.py
+++ math/bayesian_prob/0-likelihood.py
@@ -13,9 +13,9 @@ def likelihood(x, n, P):
             probabilities of developing severe side effects.
         Returns: a 1D numpy.ndarray containing the likelihood of obtaining
             the data, x and n, for each probability in P, respectively"""
-    if n < 0:
+    if type(n) is not int or n <= 0:
         raise ValueError("n must be a positive integer")
-    if x >= 0:
+    if type(n) is not int or x < 0:
         raise ValueError(
             "x must be an integer that is greater than or equal to 0")
     if x > n:
@@ -25,5 +25,5 @@ def likelihood(x, n, P):
     if not np.all((P >= 0) & (P <= 1)):
         raise ValueError('All values in P must be in the range [0, 1]')
 
-    likelihoods = np.math.comb(n * x) * (P ** x) * ((1 - P) ** (n - x))
+    likelihoods = np.math.comb(n, x) * (P ** x) * ((1 - P) ** (n - x))
     return likelihoods
