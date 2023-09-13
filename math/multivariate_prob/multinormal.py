ea4cb1690ec4839e2ea180b8f3c483e862d3e33f
vigobin <vgobin@outlook.com>
2023-09-12 21:14:47 -0500

    set up class instances

--- math/multivariate_prob/multinormal.py
+++ math/multivariate_prob/multinormal.py
@@ -15,3 +15,12 @@ class MultiNormal:
 
         if n < 2:
             raise ValueError("data must contain multiple data points")
+
+        self.mean = np.mean(data, axis=1, keepdims=True)
+
+        self.cov = np.zeros((d, d))
+
+        for i in range(d):
+            for j in range(d):
+                self.cov[i, j] = np.sum((data[i, :] - self.mean[i, :]) * (
+                    data[j, :] - self.mean[j, :])) / (n - 1)
