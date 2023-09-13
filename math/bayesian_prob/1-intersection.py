b080d6bd8c1dbf3b696d6ae91baff797549b3503
vigobin <vgobin@outlook.com>
2023-09-13 15:39:33 -0500

    erro checks

--- math/bayesian_prob/1-intersection.py
+++ math/bayesian_prob/1-intersection.py
@@ -23,9 +23,14 @@ def intersection(x, n, P, Pr):
         raise ValueError("x cannot be greater than n")
     if type(P) is not np.ndarray or P.ndim != 1:
         raise TypeError("P must be a 1D numpy.ndarray")
+    if type(Pr) is not np.ndarray or Pr.shape != P.shape:
+        raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
     if not np.all((P >= 0) & (P <= 1)):
         raise ValueError('All values in P must be in the range [0, 1]')
-
+    if not np.all((Pr >= 0) & (Pr <= 1)):
+        raise ValueError('All values in Pr must be in the range [0, 1]')
+    if not np.isclose(np.sum(Pr), 1):
+        raise ValueError("Pr must sum to 1")
     items = np.math.factorial(n)
     coef = items / (np.math.factorial(x) * np.math.factorial(n - x))
     likelihoods = coef * (P ** x) * ((1 - P) ** (n - x))
