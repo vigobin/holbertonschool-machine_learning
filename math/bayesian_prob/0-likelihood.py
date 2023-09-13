d857d3dbfe0f5dbcf7dc343b808097342ed1c9c5
vigobin <vgobin@outlook.com>
2023-09-13 13:44:26 -0500

    add 1-d check

--- math/bayesian_prob/0-likelihood.py
+++ math/bayesian_prob/0-likelihood.py
@@ -20,7 +20,7 @@ def likelihood(x, n, P):
             "x must be an integer that is greater than or equal to 0")
     if x > n:
         raise ValueError("x cannot be greater than n")
-    if type(P) is not np.ndarray:
+    if type(P) is not np.ndarray or P.ndim != 1:
         raise TypeError("P must be a 1D numpy.ndarray")
     if not np.all((P >= 0) & (P <= 1)):
         raise ValueError('All values in P must be in the range [0, 1]')
