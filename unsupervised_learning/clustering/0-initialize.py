a2748eba4780721cdca2582c92788b818db16772
vigobin <vgobin@outlook.com>
2023-09-25 10:21:56 -0500

    added centroids

--- unsupervised_learning/clustering/0-initialize.py
+++ unsupervised_learning/clustering/0-initialize.py
@@ -5,4 +5,16 @@ import numpy as np
 
 
 def initialize(X, k):
-    """Initializes cluster centroids for K-means"""
+    """Initializes cluster centroids for K-means.
+    Returns: a numpy.ndarray of shape (k, d) containing the
+        initialized centroids for each cluster, or None on failure"""
+    if k <= 0:
+        return None
+
+    min_values = np.min(X, axis=0)
+    max_values = np.max(X, axis=0)
+
+    centroids = np.random.uniform(low=min_values, high=max_values, size=(
+        k, X.shape[1]))
+
+    return centroids
