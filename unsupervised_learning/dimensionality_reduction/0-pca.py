7556f43fa3c97170f8dede976de9c58ed5ddcb3b
vigobin <vgobin@outlook.com>
2023-09-25 05:36:30 -0500

    build PCA

--- unsupervised_learning/dimensionality_reduction/0-pca.py
+++ unsupervised_learning/dimensionality_reduction/0-pca.py
@@ -6,3 +6,18 @@ import numpy as np
 
 def pca(X, var=0.95):
     """Performs PCA on a dataset"""
+    covariance_matrix = np.cov(X, rowvar=False)
+
+    eigenvalues, eigenvectors = np.linalg.eigh(covariance_matrix)
+
+    sorted_indices = np.argsort(eigenvalues)[::-1]
+    eigenvalues = eigenvalues[sorted_indices]
+    eigenvectors = eigenvectors[:, sorted_indices]
+
+    total_variance = np.sum(eigenvalues)
+    explained_variance = np.cumsum(eigenvalues) / total_variance
+    keep_components = np.argmax(explained_variance >= var) + 1
+
+    transformation_matrix = eigenvectors[:, :keep_components]
+
+    reduced_X = np.dot(X, transformation_matrix)
