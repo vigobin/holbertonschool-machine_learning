b9611a512836c197a194e009b549beb16cb58054
vigobin <vgobin@outlook.com>
2023-09-25 05:57:38 -0500

    used ndim

--- unsupervised_learning/dimensionality_reduction/1-pca.py
+++ unsupervised_learning/dimensionality_reduction/1-pca.py
@@ -9,4 +9,14 @@ def pca(X, ndim):
     ndim: the new dimensionality of the transformed X.
     Returns: T, a numpy.ndarray of shape (n, ndim) containing the transformed
     version of X."""
-    
+    covariance_matrix = np.cov(X, rowvar=False)
+
+    eigenvalues, eigenvectors = np.linalg.eigh(covariance_matrix)
+
+    sorted_indices = np.argsort(eigenvalues)[::-1]
+    eigenvalues = eigenvectors[sorted_indices]
+    eigenvectors = eigenvectors[:, sorted_indices]
+
+    transformation_matrix = eigenvectors[:, :ndim]
+
+    return np.dot(X, transformation_matrix)
