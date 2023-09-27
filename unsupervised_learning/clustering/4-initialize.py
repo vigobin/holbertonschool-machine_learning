84deb60e0ec072884b7d7ad10cb7fd0055f9c3a0
vigobin <vgobin@outlook.com>
2023-09-27 06:53:55 -0500

    Initialized matrices

---
+++ unsupervised_learning/clustering/4-initialize.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""Initialize GMM"""
+
+import numpy as np
+kmeans = __import__('1-kmeans').kmeans
+
+
+def initialize(X, k):
+    """Initializes variables for a Gaussian Mixture Model:
+    Returns: pi, m, S, or None, None, None on failure
+    pi is a numpy.ndarray of shape (k,) containing the priors for each 
+        cluster,initialized evenly.
+    m is a numpy.ndarray of shape (k, d) containing the centroid means for
+        each cluster, initialized with K-means.
+    S is a numpy.ndarray of shape (k, d, d) containing the covariance matrices
+        for each cluster, initialized as identity matrices."""
+    if k <= 0:
+        return None, None, None
+    
+    n, d = X.shape
+    pi = np.full(k, 1 / k)
+    centroids, _ = kmeans(X, k)
+    m = centroids
+
+    S = np.zeros((k, d, d))
+    for i in range(k):
+        S[i] = np.identity(d)
+        
+    return pi, m, S
