63724ebce8e57675d17de7426c13dd6e5ccd5214
vigobin <vgobin@outlook.com>
2023-09-27 06:56:25 -0500

    can't use loops

--- unsupervised_learning/clustering/4-initialize.py
+++ unsupervised_learning/clustering/4-initialize.py
@@ -8,7 +8,7 @@ kmeans = __import__('1-kmeans').kmeans
 def initialize(X, k):
     """Initializes variables for a Gaussian Mixture Model:
     Returns: pi, m, S, or None, None, None on failure
-    pi is a numpy.ndarray of shape (k,) containing the priors for each 
+    pi is a numpy.ndarray of shape (k,) containing the priors for each
         cluster,initialized evenly.
     m is a numpy.ndarray of shape (k, d) containing the centroid means for
         each cluster, initialized with K-means.
@@ -16,14 +16,12 @@ def initialize(X, k):
         for each cluster, initialized as identity matrices."""
     if k <= 0:
         return None, None, None
-    
+
     n, d = X.shape
     pi = np.full(k, 1 / k)
     centroids, _ = kmeans(X, k)
     m = centroids
 
-    S = np.zeros((k, d, d))
-    for i in range(k):
-        S[i] = np.identity(d)
-        
+    S = np.tile(np.identity(d), (k, 1, 1))
+
     return pi, m, S
