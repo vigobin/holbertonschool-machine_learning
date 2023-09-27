6ecddcd41c764ae504be9daa1991a5a34b32810c
vigobin <vgobin@outlook.com>
2023-09-27 07:22:20 -0500

    calculate probability density

--- unsupervised_learning/clustering/5-pdf.py
+++ unsupervised_learning/clustering/5-pdf.py
@@ -15,3 +15,25 @@ def pdf(X, m, S):
     P is a numpy.ndarray of shape (n,) containing the PDF values for
         each data point
     All values in P should have a minimum value of 1e-300"""
+    if X.shape[1] != m.shape[0] or X.shape[1] != S.shape[0] or X.shape[1] != S.shape[1]:
+        return None
+    
+    d = X.shape[1]
+
+    determinant_S = np.linalg.det(S)
+    if determinant_S == 0:
+        return None
+    
+    inverse_S = np.linalg.inv(S)
+
+    difference = X - m
+
+    exponent = -0.5 * np.sum(difference.dot(inverse_S) * difference, axis=1)
+
+    norm_const = 1 / ((2 * np.pi) ** (d / 2) * np.sqrt(determinant_S))
+
+    P = norm_const * np.exp(exponent)
+
+    P[P < 1e-300] = 1e-300
+
+    return P
