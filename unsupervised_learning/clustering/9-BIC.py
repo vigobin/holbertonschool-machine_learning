d05e822820c14d9ef9927b292cf6ea151100d161
vigobin <vgobin@outlook.com>
2023-09-27 22:08:09 -0500

    Trying BIC

--- unsupervised_learning/clustering/9-BIC.py
+++ unsupervised_learning/clustering/9-BIC.py
@@ -8,7 +8,7 @@ expectation_maximization = __import__('8-EM').expectation_maximization
 def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
     """Finds the best number of clusters for a GMM using the
         Bayesian Information Criterion.
-        Returns: best_k, best_result, l, b, or None, None, None, None
+        Returns: best_k, best_result, li, bi, or None, None, None, None
             on failure.
         best_k is the best value for k based on its BIC
         best_result is tuple containing pi, m, S
@@ -24,3 +24,23 @@ def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
         p is the number of parameters required for the model
         n is the number of data points used to create the model
         li is the log likelihood of the model"""
+    if type(X) is not np.ndarray or len(X.shape) != 2:
+        return None, None, None, None
+    if type(kmin) is not int or kmin <= 0:
+        return None, None, None, None
+    if type(kmax) is not int or kmax <= 0:
+        return None, None, None, None
+    if kmax >= X.shape[0] or kmin >= X.shape[0]:
+        return None, None, None, None
+    if type(iterations) is not int or iterations <= 0:
+        return None, None, None, None
+    if type(tol) is not float or tol <= 0:
+        return None, None, None, None
+    if type(verbose) is not bool:
+        return None, None, None, None
+    best_k = []
+    best_result = []
+    li = np.zeros(kmax - kmin + 1)
+    bi = np.zeros(kmax - kmin + 1)
+
+    return best_k, best_result, li, bi
