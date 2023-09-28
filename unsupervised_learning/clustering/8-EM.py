2e2447f87c8645e2ad4c9e917ef75538d349346c
vigobin <vgobin@outlook.com>
2023-09-27 21:39:21 -0500

    Fix output

--- unsupervised_learning/clustering/8-EM.py
+++ unsupervised_learning/clustering/8-EM.py
@@ -32,13 +32,10 @@ def expectation_maximization(X, k, iterations=1000, tol=1e-5, verbose=False):
         return None, None, None, None, None
     if type(iterations) is not int or iterations <= 0:
         return None, None, None, None, None
-    if type(tol) is not float or tol < 0:
+    if type(tol) is not float or tol <= 0:
         return None, None, None, None, None
     if type(verbose) is not bool:
         return None, None, None, None, None
-
-    n, d = X.shape
-
     pi, m, S = initialize(X, k)
 
     prev_li = None
