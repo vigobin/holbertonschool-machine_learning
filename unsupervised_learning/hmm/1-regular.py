ae7a2bf31832df3f5b08fa32275e204b513c6a5b
vigobin <vgobin@outlook.com>
2023-10-02 16:46:21 -0500

    add steady state

--- unsupervised_learning/hmm/1-regular.py
+++ unsupervised_learning/hmm/1-regular.py
@@ -12,3 +12,23 @@ def regular(P):
     n is the number of states in the markov chain.
     Returns: a numpy.ndarray of shape (1, n) containing the steady state
         probabilities, or None on failure."""
+    if type(P) is not np.ndarray:
+        return None
+
+    n = P.shape[0]
+
+    if P.shape != (n, n):
+        return None
+
+    eigvals, eigvecs = np.linalg.eig(P.T)
+
+    steady_state_i = np.where(np.isclose(eigvals, 1))[0]
+
+    if len(steady_state_i) != 1:
+        return None
+    
+    steady_state_v = np.real(eigvecs[:, steady_state_i]).T
+
+    steady_state_v /= np.sum(steady_state_v)
+
+    return steady_state_v
