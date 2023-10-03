aa64ec3f274bf95277b8baceb5b12462c0dba967
vigobin <vgobin@outlook.com>
2023-10-03 11:35:41 -0500

    check absorption

--- unsupervised_learning/hmm/2-absorbing.py
+++ unsupervised_learning/hmm/2-absorbing.py
@@ -11,3 +11,19 @@ def absorbing(P):
     P[i, j] is the probability of transitioning from state i to state j.
     n is the number of states in the markov chain.
     Returns: True if it is absorbing, or False on failure."""
+    if type(P) is not np.ndarray:
+        return False
+
+    n = P.shape[0]
+
+    if P.shape != (n, n):
+        return False
+
+    Q = P[:-n, :-n]
+    identity = np.eye(Q.shape[0])
+
+    N = np.linalg.inv(np.eye(Q.shape[0]) - Q)
+
+    abso = np.all(np.isclose(N[0], 0) | np.isfinite(N[0]))
+
+    return abso
