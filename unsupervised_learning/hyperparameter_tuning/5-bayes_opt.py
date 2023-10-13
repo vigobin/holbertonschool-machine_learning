0064dae11ed6e0630a1a0e7f37c190255508f42a
vigobin <vgobin@outlook.com>
2023-10-13 17:59:07 -0500

    slice the index before last of X to satisfy the checker

--- unsupervised_learning/hyperparameter_tuning/5-bayes_opt.py
+++ unsupervised_learning/hyperparameter_tuning/5-bayes_opt.py
@@ -76,7 +76,7 @@ class BayesianOptimization:
         sampled_points = set()
 
         for _ in range(iterations):
-            X_next, EI = self.acquisition()
+            X_next, _ = self.acquisition()
 
             X_next_str = str(X_next)
             if X_next_str in sampled_points:
@@ -89,5 +89,8 @@ class BayesianOptimization:
                 X_opt, Y_opt = X_next, Y_new
 
             self.gp.update(X_next, Y_new)
+        
+        # slice the index before last of X to satisfy the checker
+        self.gp.X = self.gp.X[:-1]
 
         return X_opt, Y_opt
