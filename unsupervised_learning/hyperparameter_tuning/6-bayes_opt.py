2a338d0aab6c823243d62912ccde0b122225ffb1
vigobin <vgobin@outlook.com>
2023-10-13 19:05:51 -0500

    loss function

--- unsupervised_learning/hyperparameter_tuning/6-bayes_opt.py
+++ unsupervised_learning/hyperparameter_tuning/6-bayes_opt.py
@@ -13,11 +13,13 @@ import matplotlib.pyplot as plt
 
 data = load_iris()
 X = data.data
-y = data.target
+Y = data.target
 
-X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_val, Y_train, Y_val = train_test_split(X, Y, test_size=0.2,
+                                                  random_state=42)
 
-def optimize_Gp():
+
+def optimize_Gp(hyperparameters):
     """Script that optimizes a machine learning model using GPyOpt:
        Optimize at least 5 different hyperparameters.
             E.g. learning rate, number of units in a layer, dropout rate,
@@ -33,3 +35,21 @@ def optimize_Gp():
             the convergence.
         Your script should save a report of the optimization to the file
             'bayes_opt.txt'."""
+    learning_rate, num_units, dropout_rate, l2_weight, batch = hyperparameters
+
+    model = Sequential()
+    model.add(Dense(num_units, input_dim=4, activation='relu',
+                    kernel_regularizer='12', kernel_initializer='he_normal'))
+    model.add(Dropout(dropout_rate))
+    model.add(Dense(3, activation='softmax'))
+
+    optimizer = Adam(learning_rate)
+    model.compile(optimizer=optimizer, loss='sparse_categorical_crossentropy',
+                  metrics=['accuracy'])
+
+    checkpoint = ModelCheckpoint(
+        f"model_lr_{learning_rate}_units_{num_units}_dropout_{
+            dropout_rate}_l2_{l2_weight}_batch_{batch}.h5",
+        monitor='val_accuracy', save_best_only=True, mode='max')
+
+    
