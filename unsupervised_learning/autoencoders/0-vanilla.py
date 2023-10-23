5055659c3adc3a4f9ed5cfccba1aed1aea9108b2
vigobin <vgobin@outlook.com>
2023-10-23 12:30:32 -0500

    compile autoencoder

--- unsupervised_learning/autoencoders/0-vanilla.py
+++ unsupervised_learning/autoencoders/0-vanilla.py
@@ -20,3 +20,31 @@ def autoencoder(input_dims, hidden_layers, latent_dims):
         binary cross-entropy loss.
     All layers should use a relu activation except for the last layer in the
         decoder, which should use sigmoid."""
+    # Encoder model
+    encoder_inputs = keras.Input(shape=(input_dims,))
+    x = encoder_inputs
+    for units in hidden_layers:
+        x = keras.layers.Dense(units, activation='relu')(x)
+    latent_layer = keras.layers.Dense(latent_dims, activation='relu')(x)
+
+    encoder = keras.Model(encoder_inputs, latent_layer, name="encoder")
+
+    # Decoder model
+    decoder_inputs = keras.Input(shape=(latent_dims,))
+    x = decoder_inputs
+    for units in reversed(hidden_layers):
+        x = keras.layers.Dense(units, activation='relu')(x)
+    decoder_outputs = keras.layers.Dense(input_dims, activation='sigmoid')(x)
+
+    decoder = keras.Model(decoder_inputs, decoder_outputs, name="decoder")
+
+    # Autoencoder model
+    autoencoder_inputs = keras.Input(shape=(input_dims,))
+    encoder_output = encoder(autoencoder_inputs)
+    decoder_output = decoder(encoder_output)
+    auto = keras.Model(autoencoder_inputs, decoder_output, name="autoencoder")
+
+    # Compile it
+    auto.compile(optimizer='adam', loss='binary_crossentropy')
+
+    return encoder, decoder, auto
