42fddd517ad206a033d95f75e703be90d1294e6a
vigobin <vgobin@outlook.com>
2023-11-06 13:30:47 -0600

    Added simple rnn function

--- supervised_learning/RNNs/1-rnn.py
+++ supervised_learning/RNNs/1-rnn.py
@@ -16,4 +16,12 @@ def rnn(rnn_cell, X, h_0):
     Returns: H, Y
     H is a numpy.ndarray containing all of the hidden states.
     Y is a numpy.ndarray containing all of the outputs."""
-    
+    t, m, i = X.shape
+    _, h = h_0.shape
+    H = np.zeros((t + 1, m, h))
+    Y = np.zeros(t, m, rnn_cell.Wy.shape[1])
+    H[0] = h_0
+    for step in range(t):
+        H[step + 1], Y[step] = rnn_cell.forward(H[step], X[step])
+
+    return H, Y
