4a92592f3d3047c8b3003b0f81f4f79ff9db5f51
vigobin <vgobin@outlook.com>
2023-11-06 13:32:41 -0600

    correct data type

--- supervised_learning/RNNs/1-rnn.py
+++ supervised_learning/RNNs/1-rnn.py
@@ -19,7 +19,7 @@ def rnn(rnn_cell, X, h_0):
     t, m, i = X.shape
     _, h = h_0.shape
     H = np.zeros((t + 1, m, h))
-    Y = np.zeros(t, m, rnn_cell.Wy.shape[1])
+    Y = np.zeros((t, m, rnn_cell.Wy.shape[1]))
     H[0] = h_0
     for step in range(t):
         H[step + 1], Y[step] = rnn_cell.forward(H[step], X[step])
