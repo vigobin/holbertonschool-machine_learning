fb0468ba6a1bb46144b5d6c39907500ee089dffe
vigobin <vgobin@outlook.com>
2023-11-07 15:47:00 -0600

    added functions

--- supervised_learning/RNNs/3-lstm_cell.py
+++ supervised_learning/RNNs/3-lstm_cell.py
@@ -23,6 +23,21 @@ class LSTMCell:
             in the order listed above.
         The weights will be used on the right side for matrix multiplication.
         The biases should be initialized as zeros."""
+        self.Wf = np.random.normal(size=(i + h, h))
+        self.Wu = np.random.normal(size=(i + h, h))
+        self.Wc = np.random.normal(size=(i + h, h))
+        self.Wo = np.random.normal(size=(i + h, h))
+        self.Wy = np.random.normal(size=(i + h, h))
+        self.bf = np.zeros((1, h))
+        self.bu = np.zeros((1, h))
+        self.bc = np.zeros((1, h))
+        self.bo = np.zeros((1, h))
+        self.by = np.zeros((1, h))
+
+    def softmax(self, x):
+        """Softmax function"""
+        output = np.exp(x - np.max(x))
+        return output / output.sum(axis=1, keepdims=True)
 
     def forward(self, h_prev, c_prev, x_t):
         """Performs forward propagation for one time step
@@ -38,3 +53,13 @@ class LSTMCell:
         h_next is the next hidden state.
         c_next is the next cell state.
         y is the output of the cell."""
+        input = np.concatenate((h_prev, x_t), axis=1)
+        forget_gate = np.dot(input, self.Wf) + self.bf
+        update = np.dot(input, self.Wu) + self.bu
+        cell_state = np.tanh(np.dot(input, self.Wc) + self.bc)
+        cell_next = forget_gate * c_prev + update * cell_state
+        output_gate = np.dot(input, self.Wo) + self.bo
+        h_next = output_gate * np.tanh(cell_next)
+        y = self.softmax(np.dot(h_next, self.Wy) + self.by)
+        
+        return h_next, cell_next, y
