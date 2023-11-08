7bb91fd3ae9604ff6d05db5ba278fa1fe98ee319
vigobin <vgobin@outlook.com>
2023-11-08 11:59:44 -0600

    indentation

--- supervised_learning/RNNs/7-bi_output.py
+++ supervised_learning/RNNs/7-bi_output.py
@@ -55,15 +55,14 @@ class BidirectionalCell:
 
         return h_prev
 
-
-def output(self, H):
-    """Calculates all outputs for the RNN:
-        H is a numpy.ndarray of shape (t, m, 2 * h) that contains the
-            concatenated hidden states from both directions,
-            excluding their initialized states.
-        t is the number of time steps.
-        m is the batch size for the data.
-        h is the dimensionality of the hidden states.
-        Returns: Y, the outputs."""
-    Y = np.dot(H, self.Wy) + self.by
-    return Y
+    def output(self, H):
+        """Calculates all outputs for the RNN:
+            H is a numpy.ndarray of shape (t, m, 2 * h) that contains the
+                concatenated hidden states from both directions,
+                excluding their initialized states.
+            t is the number of time steps.
+            m is the batch size for the data.
+            h is the dimensionality of the hidden states.
+            Returns: Y, the outputs."""
+        Y = np.dot(H, self.Wy) + self.by
+        return Y
