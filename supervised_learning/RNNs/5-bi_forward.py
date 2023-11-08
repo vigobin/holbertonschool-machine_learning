dad6e6f81dafb49fec04565219e9db6d55f0da32
vigobin <vgobin@outlook.com>
2023-11-07 20:45:11 -0600

    added init and forward prop

--- supervised_learning/RNNs/5-bi_forward.py
+++ supervised_learning/RNNs/5-bi_forward.py
@@ -20,6 +20,12 @@ class BidirectionalCell:
             in the order listed above.
         The weights will be used on the right side for matrix multiplication.
         The biases should be initialized as zeros."""
+        self.Whf = np.random.normal(size=(i + h, h))
+        self.Whb = np.random.normal(size=(i + h, h))
+        self.Wy = np.random.normal(size=(2 * h, o))
+        self.bhf = np.zeros((1, h))
+        self.bhb = np.zeros((1, h))
+        self.by = np.zeros((1, o))
 
     def forward(self, h_prev, x_t):
         """Calculates the hidden state in the forward direction
@@ -30,3 +36,7 @@ class BidirectionalCell:
             h_prev is a numpy.ndarray of shape (m, h) containing the previous
                 hidden state.
             Returns: h_next, the next hidden state."""
+        input = np.concatenate((h_prev, x_t), axis=1)
+        h_next = np.tanh(np.dot(input, self.Whf) + self.bhf)
+
+        return h_next
