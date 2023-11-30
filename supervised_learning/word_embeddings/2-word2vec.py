e79b27ed12707917f4d9b64fa56c14a5ed8b0b9d
vigobin <vgobin@outlook.com>
2023-11-30 11:18:58 -0600

    convert to keras

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -22,7 +22,7 @@ def word2vec_model(sentences, size=100, min_count=5, window=5, negative=5,
         Returns: the trained model."""
     sg = 0 if cbow else 1
 
-    model = Word2Vec(sentences, vector_size=size, min_count=min_count, window=window, 
+    model = Word2Vec(sentences, vector_size=size, min_count=min_count, window=window,
                      negative=negative, sg=sg, epochs=iterations, seed=seed, workers=workers)
 
     return model
--- supervised_learning/word_embeddings/3-gensim_to_keras.py
+++ supervised_learning/word_embeddings/3-gensim_to_keras.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Extract Word2Vec"""
 
+from keras.layers import Embedding
 import numpy as np
 
 
@@ -8,3 +9,14 @@ def gensim_to_keras(model):
     """Converts a gensim word2vec model to a keras Embedding layer.
         model is a trained gensim word2vec models
         Returns: the trainable keras Embedding."""
+    vocab_size = len(model.wv.key_to_index) + 1
+    vector_size = model.vector_size
+
+    weight_matrix = np.zeros((vocab_size, vector_size))
+
+    for word, i in model.wv.key_to_index.items():
+        weight_matrix[i] = model.wv[word]
+
+    embedding_layer = Embedding(input_dim=vocab_size, output_dim=vector_size, weights=[weight_matrix], trainable=True)
+
+    return embedding_layer
