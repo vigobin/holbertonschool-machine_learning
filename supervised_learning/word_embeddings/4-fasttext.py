1b5340061cdc93754c368f8e470a8ab69e5d6377
vigobin <vgobin@outlook.com>
2023-11-30 11:29:44 -0600

    use FastText

--- supervised_learning/word_embeddings/4-fasttext.py
+++ supervised_learning/word_embeddings/4-fasttext.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 """FastText function"""
 
-import numpy as np
+from gensim.models import FastText
 
 
-def fasttext_model(sentences, size=100, min_count=5, negative=5, window=5, cbow=True, iterations=5, seed=0, workers=1):
+def fasttext_model(sentences, size=100, min_count=5,
+                   negative=5, window=5, cbow=True, iterations=5,
+                   seed=0, workers=1):
     """creates and trains a genism fastText model:
         sentences is a list of sentences to be trained on.
         size is the dimensionality of the embedding layer.
@@ -19,3 +21,9 @@ def fasttext_model(sentences, size=100, min_count=5, negative=5, window=5, cbow=
         seed is the seed for the random number generator.
         workers is the number of worker threads to train the model.
         Returns: the trained model."""
+    sg = 0 if cbow else 1
+
+    model = FastText(sentences, size=size, min_count=min_count, window=window, 
+                     negative=negative, sg=sg, iter=iterations, seed=seed, workers=workers)
+
+    return model
