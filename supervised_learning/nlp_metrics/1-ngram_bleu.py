1124d4dd64d25936cea17374cb30890993b75b2d
vigobin <vgobin@outlook.com>
2023-12-05 01:21:52 -0600

    trying unigram

--- supervised_learning/nlp_metrics/1-ngram_bleu.py
+++ supervised_learning/nlp_metrics/1-ngram_bleu.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """N-gram BLEU score"""
 
+import numpy as np
+
 
 def ngram_bleu(references, sentence, n):
     """Calculates the n-gram BLEU score for a sentence:
@@ -9,4 +11,25 @@ def ngram_bleu(references, sentence, n):
         sentence is a list containing the model proposed sentence.
         n is the size of the n-gram to use for evaluation.
         Returns: the n-gram BLEU score."""
-    
+    sentence_counts = {word: sentence.count(word) for word in sentence}
+    max_counts = {}
+
+    for ref in references:
+        ref_counts = {word: ref.count(word) for word in ref}
+        for word in ref_counts:
+            max_counts[word] = max(max_counts.get(word, 0), ref_counts[word])
+
+    clipped_counts = {word: min(count, max_counts.get(word, 0)) for word,
+                      count in sentence_counts.items()}
+
+    bleu_score = sum(clipped_counts.values()) / max(sum(
+        sentence_counts.values()), 1)
+
+    closest_ref_len = min(len(ref) for ref in references)
+
+    if len(sentence) > closest_ref_len:
+        brevity_penalty = 1
+    else:
+        brevity_penalty = np.exp(1 - closest_ref_len / len(sentence))
+
+    return brevity_penalty * bleu_score
