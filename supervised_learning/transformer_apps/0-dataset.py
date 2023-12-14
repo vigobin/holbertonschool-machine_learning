4c047adbde1d4ad0573b755887ec4a41f174a02a
vigobin <vgobin@outlook.com>
2023-12-14 16:46:44 -0600

    cleaner init instance attributes

--- supervised_learning/transformer_apps/0-dataset.py
+++ supervised_learning/transformer_apps/0-dataset.py
@@ -12,12 +12,19 @@ class Dataset:
                 tf.data.Dataset train split, loaded as_supervided.
             data_valid, which contains the ted_hrlr_translate/pt_to_en
                 tf.data.Dataset validate split, loaded as_supervided.
-            tokenizer_pt is the Portuguese tokenizer created from the training set
-            tokenizer_en is the English tokenizer created from the training set."""
-        examples, metadata = tfds.load(
-            'ted_hrlr_translate/pt_to_en', with_info=True, as_supervised=True)
-        self.data_train, self.data_valid = examples[
-            'train'], examples['validation']
+            tokenizer_pt is the Portuguese tokenizer created from
+                the training set.
+            tokenizer_en is the English tokenizer created from
+                the training set."""
+        self.data_train = tfds.load('ted_hrlr_translate/pt_to_en',
+                                    split='train', as_supervised=True)
+        self.data_valid = tfds.load('ted_hrlr_translate/pt_to_en',
+                                    split='validation', as_supervised=True)
+
+        self.tokenizer_pt, self.tokenizer_en = self.tokenize_dataset(
+            self.data_train)
+        self.tokenizer_pt, self.tokenizer_en = self.tokenize_dataset(
+            self.data_train)
 
     def tokenize_dataset(self, data):
         """Creates sub-word tokenizers for our dataset:
@@ -29,10 +36,10 @@ class Dataset:
             Returns: tokenizer_pt, tokenizer_en:
             tokenizer_pt is the Portuguese tokenizer.
             tokenizer_en is the English tokenizer."""
-        tokenizer_en = tfds.deprecated.text.SubwordTextEncoder.build_from_corpus(
-            (en.numpy() for pt, en in data), target_vocab_size=2**15)
-
-        tokenizer_pt = tfds.deprecated.text.SubwordTextEncoder.build_from_corpus(
-            (pt.numpy() for pt, en in data), target_vocab_size=2**15)
-
+        tokenizer_en = tfds.deprecated.text.SubwordTextEncoder\
+            .build_from_corpus((en.numpy() for pt, en in data),
+                               target_vocab_size=2**15)
+        tokenizer_pt = tfds.deprecated.text.SubwordTextEncoder\
+            .build_from_corpus((pt.numpy() for pt, en in data),
+                               target_vocab_size=2**15)
         return tokenizer_pt, tokenizer_en
