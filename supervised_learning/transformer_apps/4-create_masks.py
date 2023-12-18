183d1e232bfeed836c370f937ce4f467b771c14c
vigobin <vgobin@outlook.com>
2023-12-18 11:00:51 -0600

    buid masks

--- supervised_learning/transformer_apps/4-create_masks.py
+++ supervised_learning/transformer_apps/4-create_masks.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 """Create Masks"""
 
+import tensorflow.compat.v2 as tf
+import tensorflow_datasets as tfds
+
 
 def create_masks(inputs, target):
     """Creates all masks for training/validation:
@@ -21,12 +24,16 @@ def create_masks(inputs, target):
         decoder_mask is the tf.Tensor padding mask of shape
             (batch_size, 1, 1, seq_len_in) used in the 2nd attention
             block in the decoder."""
-    encoder_mask = 
+    encoder_mask = tf.cast(tf.math.equal(
+        inputs, 0), tf.float32)[..., tf.newaxis, tf.newaxis]
 
-    decoder_mask = 
+    decoder_mask = tf.cast(tf.math.equal(
+        inputs, 0), tf.float32)[..., tf.newaxis, tf.newaxis]
 
-    look_ahead_mask = create_look_ahead_mask(tf.shape(target)[1])
-    dec_target_padding_mask = create_padding_mask(target)
+    look_ahead_mask = 1 - tf.linalg.band_part(tf.ones((tf.shape(
+        target)[1], tf.shape(target)[1])), -1, 0)
+    dec_target_padding_mask = tf.cast(tf.math.equal(
+        target, 0), tf.float32)[..., tf.newaxis, tf.newaxis]
     combined_mask = tf.maximum(dec_target_padding_mask, look_ahead_mask)
 
     return encoder_mask, combined_mask, decoder_mask
