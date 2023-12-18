a899eca22c382b5a0bdfd8609e8e7afddc91c903
vigobin <vgobin@outlook.com>
2023-12-18 16:17:28 -0600

    Task 2

---
+++ supervised_learning/qa_bot/2-qa.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Answer Questions"""
+
+
+def answer_loop(reference):
+    """Answers questions from a reference text:
+        reference is the reference text.
+        If the answer cannot be found in the reference text,
+        respond with Sorry, I do not understand your question."""
+    
