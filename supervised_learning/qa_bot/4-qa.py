5a1c4ed7f530770bf93ea1a7c86df6f72eb6c3a3
vigobin <vgobin@outlook.com>
2023-12-19 16:00:50 -0600

    Task 4

---
+++ supervised_learning/qa_bot/4-qa.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Multi-reference Question Answering"""
+
+
+def question_answer(coprus_path):
+    """Answers questions from multiple reference texts:
+        corpus_path is the path to the corpus of reference documents."""
+    
+
